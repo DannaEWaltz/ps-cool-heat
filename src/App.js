5f05751ce67b73a25b53dbce9df5a38c51b73de9
Danna Waltz <dannaewaltz@gmail.com>
2024-04-02 19:50:06 -0600

    styling

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <img className="banner" src={banner} alt="banner"/>
       {/* <img className="logo" src={logo} alt="PSCH"/> */}
       <header className="header">
-        <h1>~    ~    ~  PS Cool Heat  ~    ~    ~</h1>
+        {/* <h1>~    ~    ~  PS Cool Heat  ~    ~    ~</h1> */}
         <h2>1745 Shea Center Drive #449, Highlands Ranch, CO 80129</h2>
         <h1>720-285-0145</h1>
         <p className="construct">
