64f611fc50068ae007de1eeee3ea689214101289
Danna Waltz <dannaewaltz@gmail.com>
2024-04-02 19:32:14 -0600

    banner

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Form from './Form';
-import logo from './psch.png';
+// import logo from './psch.png';
+import banner from './BannerHead.png';
 
 function App() {
   return (
     <div className="background">
-      <img className="logo" src={logo} alt="PSCH"/>
+      <img className="banner" src={banner} alt="banner"/>
+      {/* <img className="logo" src={logo} alt="PSCH"/> */}
       <header className="header">
         <h1>~    ~    ~  PS Cool Heat  ~    ~    ~</h1>
         <h2>1745 Shea Center Drive #449, Highlands Ranch, CO 80129</h2>
         <h1>720-285-0145</h1>
-        <h3>HVAC: Installation, Maintenance, Repair 24/7</h3>
         <p className="construct">
           Our website is under construction.
           Until our new design is ready, please fill out the form below so we may contact you!
